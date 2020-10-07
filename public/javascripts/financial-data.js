7f0b1d4270a6a0c33335f04691d42d1218bb6b97
abernier <antoine.bernier@gmail.com>
2020-10-07 11:32:06 +0200

    Iteration 3 and 4

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,33 +1,53 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
-// Our query-string parameters
-const start = '2019-10-07'
-const end = '2020-10-07'
-const currency = 'EUR'
+const $start = document.querySelector('input[name="start"]')
+const $end = document.querySelector('input[name="end"]')
+const $currency = document.querySelector('select[name="currency"]')
 
-axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}&currency=${currency}`)
-  .then(response => {
-    console.log(response.data)
+// Draw an empty line-chart
+const myChart = new Chart(ctx, {
+  type: 'line',
+    data: {
+      labels: [], // empty labels
+      datasets: [{
+        label: `bitcoin value`,
+        data: [] // empty datas
+      }]
+    }
+});
 
-    // Extract datas from response
-    const labels = Object.keys(response.data.bpi)
-    const data = Object.values(response.data.bpi)
+$start.addEventListener('change', getDatasAndUpdateChart)
+$end.addEventListener('change', getDatasAndUpdateChart)
+$currency.addEventListener('change', getDatasAndUpdateChart)
 
-    // Draw a line-chart
-    const myChart = new Chart(ctx, {
-      type: 'line',
-        data: {
-          labels,
-          datasets: [{
-            label: `bitcoin value`,
-            data
-          }]
-        }
-    });
-  })
-  .catch(err => {
-    console.log('error while fetching coindesk datas', err)
-  })
+function getDatasAndUpdateChart() {
+  // Our query-string parameters
+  const start = `${$start.value}-01`
+  const end = `${$end.value}-01`
+  const currency = $currency.value
+
+  axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}&currency=${currency}`)
+    .then(response => {
+      console.log(response.data)
+
+      // Extract datas from API response
+      const labels = Object.keys(response.data.bpi)
+      const data = Object.values(response.data.bpi)
+
+      // Update the line-chart with fresh datas
+      myChart.data.labels = labels
+      myChart.data.datasets[0].data = data
+      myChart.update()
+      
+    })
+    .catch(err => {
+      console.log('error while fetching coindesk datas', err)
+    })
+}
+
+getDatasAndUpdateChart()
+
+
 
   
   
