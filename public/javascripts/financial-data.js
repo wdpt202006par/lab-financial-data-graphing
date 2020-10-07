5f0b611944eb0c0ffcd76345f47ede6357156d72
abernier <antoine.bernier@gmail.com>
2020-10-07 10:52:23 +0200

    Iteration 2

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,7 +1,33 @@
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+const ctx = document.getElementById('myChart').getContext('2d');
+
+// Our query-string parameters
+const start = '2019-10-07'
+const end = '2020-10-07'
+const currency = 'EUR'
+
+axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}&currency=${currency}`)
   .then(response => {
     console.log(response.data)
+
+    // Extract datas from response
+    const labels = Object.keys(response.data.bpi)
+    const data = Object.values(response.data.bpi)
+
+    // Draw a line-chart
+    const myChart = new Chart(ctx, {
+      type: 'line',
+        data: {
+          labels,
+          datasets: [{
+            label: `bitcoin value`,
+            data
+          }]
+        }
+    });
   })
   .catch(err => {
     console.log('error while fetching coindesk datas', err)
   })
+
+  
+  
